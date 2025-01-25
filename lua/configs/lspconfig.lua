2175c3f5a59fccaa4c0722f3bc6cfb89f8d801f8
Jan Cichomski <yancichomski@gmail.com>
2025-01-25 16:21:02 +0100

    Remove ruff_lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "hls", "ruff_lsp", "ruff", "pyright", "gopls" }
+local servers = { "html", "cssls", "hls", "ruff", "pyright", "gopls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
