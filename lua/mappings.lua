02b0831ce9f7da70e5043d6e0a6ab5908471fdf7
Jan Cichomski <yancichomski@gmail.com>
2025-03-20 12:57:12 +0100

    save

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,11 +15,11 @@ vim.keymap.del("n", "<leader>v")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 -- vim.keymap.del("n", "<leader>b")
 --
-local dap = require("dap")
+local dap = require "dap"
 -- TODO: step over etc not working as terminal intercepts them...
-vim.keymap.set('n', '<F5>', dap.continue)
-vim.keymap.set('n', '<leader><F5>', dap.run_last)
-vim.keymap.set('n', '<F4>', dap.terminate)
+vim.keymap.set("n", "<F5>", dap.continue)
+vim.keymap.set("n", "<leader><F5>", dap.run_last)
+vim.keymap.set("n", "<F4>", dap.terminate)
 -- vim.keymap.set('n', '<F5>', function()
 --   -- (Re-)reads launch.json if present
 --   if vim.fn.filereadable(".vscode/launch.json") then
@@ -27,14 +27,18 @@ vim.keymap.set('n', '<F4>', dap.terminate)
 --   end
 --   require("dap").continue()
 -- end)
-vim.keymap.set('n', '<F9>', dap.step_over)
-vim.keymap.set('n', '<F11>', dap.step_into)
-vim.keymap.set('n', '<F12>', dap.step_out)
+vim.keymap.set("n", "<F9>", dap.step_over)
+vim.keymap.set("n", "<F11>", dap.step_into)
+vim.keymap.set("n", "<F12>", dap.step_out)
 vim.keymap.del("n", "<leader>b")
-vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
+vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
 
 -- This maps space-hr to revert/undo the git hunk under your cursor
-vim.keymap.set('n', '<space>hr', ':Gitsigns reset_hunk<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<space>hr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true })
 
 -- This lets you revert specific lines you've selected in visual mode
-vim.keymap.set('v', '<space>hr', ':Gitsigns reset_hunk<CR>', { noremap = true, silent = true })
+vim.keymap.set("v", "<space>hr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-\\>", "<cmd>vsplit<CR>")
+
+vim.keymap.set("n", "<C-w>", "<cmd>q<CR>")
