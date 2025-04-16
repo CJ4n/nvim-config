e8eca6340dec75a3d2db1408bf35814652c42e77
Jan Cichomski <yancichomski@gmail.com>
2025-04-16 23:13:38 +0200

    Fixed pyright (disabled ruff altogheter :(

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
-local servers = { "html", "cssls", "hls", "pyright", "ruff", "gopls", "templ" }
+local servers = { "html", "cssls", "hls", "pyright", "gopls", "templ" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 for _, lsp in ipairs(servers) do
@@ -12,84 +12,84 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+--
+-- lspconfig.ruff.setup {
+--   init_options = {
+--     settings = {
+--       -- Add any extra CLI arguments for Ruff here
+--       args = {},
+--     },
+--   },
+--   on_attach = function(client, bufnr)
+--     client.server_capabilities.hoverProvider = false
+--   end,
+-- }
+-- require("lspconfig").pyright.setup {
+--   settings = {
+--     pyright = {
+--       -- Using Ruff's import organizer
+--       disableOrganizeImports = true,
+--     },
+--     python = {
+--       analysis = {
+--         -- Ignore all files for analysis to exclusively use Ruff for linting
+--         -- ignore = { "*" },
+--       },
+--     },
+--   },
+-- }
+--
+-- lspconfig.html.setup {
+--   filetypes = { "html", "tmpl" },
+--   cmd = { "vscode-html-language-server", "--stdio" },
+--   init_options = {
+--     configurationSection = { "html", "css", "javascript" },
+--     embeddedLanguages = {
+--       css = true,
+--       javascript = true,
+--       -- go = true,
+--     },
+--     provideFormatter = true,
+--   },
+--   settings = {
+--     html = {
+--       format = {
+--         templating = true,
+--         wrapLineLength = 120,
+--         indentInnerHtml = true,
+--       },
+--       validate = {
+--         scripts = true,
+--         styles = true,
+--         templating = true,
+--       },
+--     },
+--   },
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
 
-lspconfig.ruff.setup {
-  init_options = {
-    settings = {
-      -- Add any extra CLI arguments for Ruff here
-      args = {},
-    },
-  },
-  on_attach = function(client, bufnr)
-    client.server_capabilities.hoverProvider = false
-  end,
-}
-require("lspconfig").pyright.setup {
-  settings = {
-    pyright = {
-      -- Using Ruff's import organizer
-      disableOrganizeImports = true,
-    },
-    python = {
-      analysis = {
-        -- Ignore all files for analysis to exclusively use Ruff for linting
-        -- ignore = { "*" },
-      },
-    },
-  },
-}
-
-lspconfig.html.setup {
-  filetypes = { "html", "tmpl" },
-  cmd = { "vscode-html-language-server", "--stdio" },
-  init_options = {
-    configurationSection = { "html", "css", "javascript" },
-    embeddedLanguages = {
-      css = true,
-      javascript = true,
-      -- go = true,
-    },
-    provideFormatter = true,
-  },
-  settings = {
-    html = {
-      format = {
-        templating = true,
-        wrapLineLength = 120,
-        indentInnerHtml = true,
-      },
-      validate = {
-        scripts = true,
-        styles = true,
-        templating = true,
-      },
-    },
-  },
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
-
-lspconfig.gopls.setup {
-  filetypes = { "go", "html", "tmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      templateExtensions = { "html", "tmpl", "gotmpl" },
-      analyses = {
-        templatecheck = true,
-        shadow = true,
-      },
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        constantValues = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-    },
-  },
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
+-- lspconfig.gopls.setup {
+--   filetypes = { "go", "html", "tmpl" },
+--   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+--   settings = {
+--     gopls = {
+--       templateExtensions = { "html", "tmpl", "gotmpl" },
+--       analyses = {
+--         templatecheck = true,
+--         shadow = true,
+--       },
+--       hints = {
+--         assignVariableTypes = true,
+--         compositeLiteralFields = true,
+--         constantValues = true,
+--         parameterNames = true,
+--         rangeVariableTypes = true,
+--       },
+--     },
+--   },
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
